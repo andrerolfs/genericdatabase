54e17090e3884783a2553924a5882da8a7bb4b9f
Andre Rolfs <andre.rolfs@wartbar.de>
2017-03-04 14:30:02 +0100

    removed outdated code

--- src/main/java/com/wartbar/genericdb/main/GenericDBMain.java
+++ src/main/java/com/wartbar/genericdb/main/GenericDBMain.java
@@ -23,71 +23,6 @@ import java.util.List;
  */
 public class GenericDBMain {
 
-	public static void testDirect() {
-		System.out.println("Hello world1");
-
-		ArrayList<DBData> dataList = new ArrayList<>();
-		dataList.add(new DBData(1,1,2));
-		dataList.add(new DBData(2,4,3));
-		dataList.add(new DBData(3,2,7));
-
-		ArrayList<DBString> stringList = new ArrayList<>();
-		stringList.add(new DBString(4,5,"Hello"));
-		stringList.add(new DBString(5,6,"world"));
-
-		DBEntry entry = new DBEntry();
-		entry.setDataList(dataList);
-		entry.setStringList(stringList);
-		entry.setTimestamp(Calendar.getInstance());
-
-		for (DBData dataObject : dataList) {
-			dataObject.setEntry(entry);
-		}
-
-		for (DBString stringObject : stringList) {
-			stringObject.setEntry(entry);
-		}
-
-		try {
-			DB.beginTransaction();
-			DB.persist(entry);
-			System.out.println("Hello world 2");
-
-			for (DBData data : dataList) {
-				DB.persist(data);
-			}
-			System.out.println("Hello world 3");
-
-			for (DBString string : stringList) {
-				DB.persist(string);
-			}
-			System.out.println("Hello world 4");
-
-			DB.commitTransaction();
-		} catch (Exception e) {
-			System.out.println("An exception occured while adding");
-			System.out.println(e.getMessage());
-			DB.rollbackTransaction();
-		}
-
-		List<DBEntry> entries = null;
-
-		try {
-			DB.beginTransaction();
-			TypedQuery<DBEntry> query = DB.getEntityManager().createQuery("SELECT e from DBEntry e", DBEntry.class);
-			entries = query.getResultList();
-			DB.commitTransaction();
-		} catch (Exception e) {
-			System.out.println("An exception occured while query");
-			System.out.println(e.getMessage());
-			DB.rollbackTransaction();
-		}
-
-		for (DBEntry e : entries) {
-			System.out.println(e.getTimestamp().getTime());
-		}
-	}
-
 	public static Entry createTestEntry() {
 		Entry entry = Entry.createNew();
 		entry.addData(1,1,1);
@@ -110,19 +45,11 @@ public class GenericDBMain {
 
 		List<DBEntry> entries = IO.getAllEntries();
 
-		/*
-		System.out.println("---> print all");
-		for (DBEntry entry : entries) {
-			Entry.createFromExisting(entry).print();
-		}
-		System.out.println("<--- print all");
-*/
 		Long firstEntryID = entries.get(0).getEntryId();
 		DBEntry entry = IO.getEntry(firstEntryID);
 		System.out.println("---> First entry in DB is :");
 		Entry.createFromExisting(entry).print();
 		System.out.println("<--- end of first entry in DB");
-
 
 		entries = IO.getEntriesOfToday();
 
@@ -133,13 +60,10 @@ public class GenericDBMain {
 		System.out.println("<---#### today");
 
 		System.out.println("today was " + Time.getYesterday().getTime());
-
-
 	}
 
 	public static void main(String[] args) {
 		testViaIO();
-		//testDirect();
 	}
 
 }
